91edb2d29b3fcb7e4898288ed4a32d4b9be87b04
Brandon <brandonlee503@gmail.com>
2015-11-30 11:47:02 -0800

    Added DirectoryEntity Model

---
+++ src/main/groovy/edu/oregonstate/mist/api/core/DirectoryEntity.groovy
@@ -0,0 +1,19 @@
+package edu.oregonstate.mist.api.core
+
+/**
+ * DirectoryEntity representation class.
+ */
+class DirectoryEntity {
+    String fullName
+    String primaryAffiliation
+    String title
+    String department
+    String departmentMailingAddress
+    String homePhoneNumber
+    String homeAddress
+    String officePhoneNumber
+    String officeAddress
+    String faxNumber
+    String emailAddress
+    String username
+}
