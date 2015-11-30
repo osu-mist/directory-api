45654bb402d622f7a7c33aea12f44c5f8f04ddbb
Brandon <brandonlee503@gmail.com>
2015-11-30 11:13:20 -0800

    Added person class

---
+++ src/main/groovy/edu/oregonstate/mist/api/core/Person.groovy
@@ -0,0 +1,14 @@
+package edu.oregonstate.mist.api.core
+
+/**
+ * Person representation class.
+ */
+class Person {
+    String firstName
+    String lastName
+    String osuID
+    String username
+    String email
+    String eppn
+    String upn
+}
