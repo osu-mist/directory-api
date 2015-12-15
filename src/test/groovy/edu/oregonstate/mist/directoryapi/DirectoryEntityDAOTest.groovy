7fe975bdb251b5747a476012419aeff4d64834ef
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-12-15 14:33:25 -0800

    test private object
    co-276

--- src/test/groovy/edu/oregonstate/mist/directoryapi/DirectoryEntityDAOTest.groovy
+++ src/test/groovy/edu/oregonstate/mist/directoryapi/DirectoryEntityDAOTest.groovy
@@ -4,12 +4,13 @@ import org.junit.Test
 import static org.junit.Assert.*
 
 class DirectoryEntityDAOTest {
+    private static final DirectoryEntityDAO directoryEntityDAO = new DirectoryEntityDAO()
     private static final Long goodOSUUID = 51646559347
     private static final Long badOSUUID = 0
 
     @Test
     public void testGetByOSUUID() {
-        assertNotNull(new DirectoryEntityDAO().getByOSUUID(goodOSUUID))
-        assertNull(new DirectoryEntityDAO().getByOSUUID(badOSUUID))
+        assertNotNull(directoryEntityDAO.getByOSUUID(goodOSUUID))
+        assertNull(directoryEntityDAO.getByOSUUID(badOSUUID))
     }
 }
