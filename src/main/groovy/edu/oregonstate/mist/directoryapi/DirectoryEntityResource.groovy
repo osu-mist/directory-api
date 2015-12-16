c4df96fe2ad4e372f8743954867342c09789e385
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-12-15 16:31:56 -0800

    implement prototype of dummy resource
    co-275

--- src/main/groovy/edu/oregonstate/mist/directoryapi/DirectoryEntityResource.groovy
+++ src/main/groovy/edu/oregonstate/mist/directoryapi/DirectoryEntityResource.groovy
@@ -3,43 +3,61 @@ package edu.oregonstate.mist.directoryapi
 import edu.oregonstate.mist.api.Resource
 import edu.oregonstate.mist.api.AuthenticatedUser
 import io.dropwizard.auth.Auth
+import io.dropwizard.jersey.params.IntParam
+
+import javax.print.attribute.standard.Media
 import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.Consumes
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.Response.ResponseBuilder
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.UriInfo
 
 /**
- * Sample resource class.
+ * DirectoryEntity Resource class.
  */
-@Path('/sample/')
+@Path('/directory')
 class DirectoryEntityResource extends Resource {
-    /**
-     * Responds to GET requests by returning a message.
-     *
-     * @return message
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response getMessage(@Auth AuthenticatedUser authenticatedUser) {
-        ResponseBuilder responseBuilder = ok(new Sample().message)
-        responseBuilder.build()
-    }
+
+    private final DirectoryEntityDAO directoryEntityDAO
+
+    @Context
+    UriInfo uriInfo
 
     /**
-     * Responds to POST requests by reading and returning a message.
-     *
-     * @param message
-     * @return message
+     * Constructs the object after receiving and storing directoryEntityDAO instance.
      */
-    @POST
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response postMessage(String message, @Auth AuthenticatedUser authenticatedUser) {
-        ResponseBuilder responseBuilder = ok(message)
-        responseBuilder.build()
+    public DirectoryEntityResource(DirectoryEntityDAO directoryEntityDAO) {
+        this.directoryEntityDAO = directoryEntityDAO
+    }
+
+    @GET
+    @Path('/')
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<DirectoryEntity> getAll() {
+        directoryEntityDAO.allDirectoryEntities
+    }
+
+    @GET
+    @Path('/{osuuid}')
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getByOsuuid(@PathParam('osuuid') IntParam osuuid) {
+        DirectoryEntity directoryEntities = directoryEntityDAO.getByosuuid(osuuid.get())
+
+        Response returnResponse
+
+        if (directoryEntities == null) {
+            edu.oregonstate.mist.api.Error returnError = new edu.oregonstate.mist.api.Error(userMessage: "Resource Not Found.", code: Response.Status.NOT_FOUND.getStatusCode())
+            returnResponse = Response.status(Response.Status.NOT_FOUND).entity(returnError).build()
+        } else {
+            returnResponse = Response.ok(directoryEntities).build()
+        }
+
+        returnResponse
     }
 }
