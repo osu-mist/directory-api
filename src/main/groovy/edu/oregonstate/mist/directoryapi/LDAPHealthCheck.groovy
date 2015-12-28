eb7164a4b7bf38ecd783bbee1842f46bb7eec2b4
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-12-28 13:39:13 -0800

    javadoc healthcheck
    co-293

--- src/main/groovy/edu/oregonstate/mist/directoryapi/LDAPHealthCheck.groovy
+++ src/main/groovy/edu/oregonstate/mist/directoryapi/LDAPHealthCheck.groovy
@@ -4,13 +4,26 @@ import com.codahale.metrics.health.HealthCheck
 import com.codahale.metrics.health.HealthCheck.Result
 import org.ldaptive.LdapException
 
+/**
+ * LDAP health check.
+ */
 class LDAPHealthCheck extends HealthCheck {
     private final DirectoryEntityDAO directoryEntityDAO
 
+    /**
+     * Constructs LDAP health check using directory entity data access object.
+     *
+     * @param directoryEntityDAO
+     */
     public LDAPHealthCheck(DirectoryEntityDAO directoryEntityDAO) {
         this.directoryEntityDAO = directoryEntityDAO
     }
 
+    /**
+     * Verifies that LDAP connection is successful.
+     *
+     * @return result
+     */
     @Override
     protected Result check() {
         try {
