67912ca07852df29ad267d8da1df97742589f65c
Brandon <brandonlee503@gmail.com>
2016-01-06 16:55:38 -0800

    Created mapper

---
+++ src/main/groovy/edu/oregonstate/mist/directoryapi/DirectoryEntityMapper.groovy
@@ -0,0 +1,12 @@
+package edu.oregonstate.mist.directoryapi
+
+import edu.oregonstate.mist.directoryapi.DirectoryEntity
+import edu.oregonstate.mist.directoryapi.ResourceObject
+
+class DirectoryEntityMapper {
+    public ResourceObject map(DirectoryEntity directoryEntity) {
+        // TODO: Define and then create attributes object here
+
+        //new ResourceObject(id: id, type: "directoryEntity", attributes: attributes)
+    }
+}
