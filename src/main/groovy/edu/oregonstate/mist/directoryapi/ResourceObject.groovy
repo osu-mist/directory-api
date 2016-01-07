bc5e9233c690e75712260f638ac63076b44283da
Brandon <brandonlee503@gmail.com>
2016-01-06 16:56:08 -0800

    Created ResourceObject for JSONAPI

---
+++ src/main/groovy/edu/oregonstate/mist/directoryapi/ResourceObject.groovy
@@ -0,0 +1,9 @@
+package edu.oregonstate.mist.directoryapi
+
+import edu.oregonstate.mist.directoryapi.DirectoryEntity
+
+class ResourceObject {
+    String id
+    String type
+    DirectoryEntity directoryEntity
+}
