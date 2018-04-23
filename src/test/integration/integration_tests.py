b495cb4434b112b1595539228eed386fba578e3c
Andrew Ekstedt <ekstedta@oregonstate.edu>
2018-04-23 16:44:14 -0700

    refactor integration tests
    Make constructing request urls a little nicer

--- src/test/integration/integration_tests.py
+++ src/test/integration/integration_tests.py
@@ -2,95 +2,104 @@
 # encoding: utf-8
 
 import unittest, sys, json, math, operator, requests
-from api_request import basic_request
-import urllib
 
 class gateway_tests(unittest.TestCase):
+    def setUp(self):
+        global access_token
+        global request_url
+        self.access_token = access_token
+        self.url = request_url
+
+    def _request(self, path, verb="get", access_token=None, **params):
+        url = self.url.rstrip("/")
+        if path:
+            url += "/" + path.lstrip("/")
+        if access_token is None:
+            access_token = self.access_token
+        headers = {'Authorization': access_token}
+        request = requests.request(verb, url, headers=headers, params=params)
+        return request
+
     def test_not_found(self):
-        bad_url = request_url + "012345678"
-        response = basic_request(bad_url, access_token)
+        bad_osuuid = "012345678"
+        response = self._request(bad_osuuid)
         self.assertEqual(response.status_code, 404)
 
     def test_overly_broad_search_term(self):
-        response = basic_request(request_url + '?q=John', access_token)
+        response = self._request("", q="John")
         self.assertEqual(response.status_code, 400)
 
     def test_user_not_found_search_term(self):
-        bad_name_url = request_url + '?q=abcdef'
-        response = basic_request(bad_name_url, access_token)
+        bad_name = 'abcdef'
+        response = self._request("", q=bad_name)
         self.assertEquals(response.status_code, 200)
         self.assertEqual(response.json()['data'], [])
 
     def test_user_found_search_term(self):
-        good_name_url = request_url + '?q=' + urllib.quote(config_json["good_name"])
-        response = basic_request(good_name_url, access_token)
+        response = self._request("", q=config_json["good_name"])
         self.assertEquals(response.status_code, 200)
         self.assertNotEqual(response.json()['data'], [])
 
     def test_user_found_search_term_reversed(self):
         name = config_json["good_name"]
         reversed_name = " ".join(reversed(name.split()))
-        good_name_url = request_url + '?q=' + urllib.quote(reversed_name)
-        response = basic_request(good_name_url, access_token)
+        response = self._request("", q=reversed_name)
         self.assertEquals(response.status_code, 200)
         self.assertNotEqual(response.json()['data'], [])
 
     def test_missing_q_search_term(self):
-        response = basic_request(request_url, access_token)
+        response = self._request("")
         self.assertEquals(response.status_code,400)
 
     def test_no_access_token_search_term(self):
-        response = basic_request(request_url, '')
+        response = self._request("", access_token="")
         self.assertEquals(response.status_code,401)
 
     def test_middle_name_search_term(self):
-        middle_name_url = request_url + "?q=" + urllib.quote(config_json["middle_cn_search"])
-        response = basic_request(middle_name_url, access_token)
+        response = self._request("", q=config_json["middle_cn_search"])
         self.assertEquals(response.status_code, 200)
         self.assertNotEqual(response.json()['data'], [])
 
     def test_partial_mail_search_term(self):
-        partial_mail_url = request_url + "?q=" + urllib.quote(config_json["partial_email"])
-        response = basic_request(partial_mail_url, access_token)
+        response = self._request("", q=config_json["partial_email"])
         self.assertEquals(response.status_code, 200)
         self.assertNotEqual(response.json()['data'], [])
 
     def test_sanitize(self):
         good_uid = "browtayl"
         for bad_char in "()*&#":
-            response = basic_request(request_url, access_token, params=dict(q=good_uid + bad_char))
+            response = self._request("", q=good_uid + bad_char)
             self.assertEquals(response.status_code, 200)
             self.assertEquals(response.json()['data'][0]['attributes']['username'], good_uid)
 
-            response = basic_request(request_url, access_token, params=dict(q=bad_char + good_uid))
+            response = self._request("", q=bad_char + good_uid)
             self.assertEquals(response.status_code, 200)
             self.assertEquals(response.json()['data'][0]['attributes']['username'], good_uid)
 
     def test_unicode(self):
         unicode_name = 'Hernández'
-        response = basic_request(request_url, access_token, params=dict(q=unicode_name))
+        response = self._request("", q=unicode_name)
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.json()['data'][0]['attributes']['username'], 'hernand2')
 
     def test_osuuid_found(self):
-        osuuid_url = request_url + "/" + config_json["good_osuuid"]
-        response = basic_request(osuuid_url, access_token)
+        good_osuuid = config_json["good_osuuid"]
+        response = self._request(good_osuuid)
         self.assertEquals(response.status_code, 200)
         self.assertNotEqual(response.json()['data'], [])
 
     def test_osuuid_bad_req_empty(self):
-        osuuid_url = request_url + "/" + ""
-        response = basic_request(osuuid_url, access_token)
+        response = self._request("/")
         self.assertEquals(response.status_code, 400)
 
     def test_osuuid_bad_req_invalid(self):
-        osuuid_url = request_url + "/" + "!@#!@#12dqwd"
-        response = basic_request(osuuid_url, access_token)
+        invalid_osuuid = "!@#!@#12dqwd"
+        response = self._request(invalid_osuuid)
         self.assertEquals(response.status_code, 404)
 
-    def test_osuuid_no_acc(self):
-        osuuid_url = request_url + "/" + config_json["good_osuuid"]
-        response = basic_request(osuuid_url, '')
+    def test_osuuid_no_access_token(self):
+        good_osuuid = config_json["good_osuuid"]
+        response = self._request(good_osuuid, access_token='')
         self.assertEquals(response.status_code, 401)
 
 if __name__ == '__main__':
