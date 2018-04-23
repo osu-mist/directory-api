6636909835f36dfe44927b13d6ea24f6903a216f
Andrew Ekstedt <ekstedta@oregonstate.edu>
2018-04-23 16:13:05 -0700

    add missing integration tests
    CO-1050
    
    Add some tests that were present in DirectoryEntityDAOTest, but not in
    integration_tests.py.
    
    Also, make sure we're actually testing what we say we're testing: when
    searching by query, the directory api return a 200 response even if
    there are 0 results. it's not enough to just test the status code, we
    also need to look at the actual result.
    
    Also, fix two tests that had the same method name, and tweak one test to
    make the formatting match the other tests.
    
    Also, fix the command line parsing.

--- src/test/integration/api_request.py
+++ src/test/integration/api_request.py
@@ -1,6 +1,6 @@
 import json, requests
 
-def basic_request(url, access_token, verb="get"):
+def basic_request(url, access_token, verb="get", params=None):
     headers = {'Authorization': access_token}
-    request = requests.request(verb, url, headers=headers)
+    request = requests.request(verb, url, headers=headers, params=params)
     return request
--- src/test/integration/integration_tests.py
+++ src/test/integration/integration_tests.py
@@ -1,22 +1,39 @@
+#!/usr/bin/env python2
+# encoding: utf-8
+
 import unittest, sys, json, math, operator, requests
-from api_request import *
+from api_request import basic_request
 import urllib
 
 class gateway_tests(unittest.TestCase):
     def test_not_found(self):
-        bad_url = url + "012345678"
-
-        self.assertEqual(basic_request
-            (bad_url, access_token).status_code, 404)
+        bad_url = request_url + "012345678"
+        response = basic_request(bad_url, access_token)
+        self.assertEqual(response.status_code, 404)
 
     def test_overly_broad_search_term(self):
         response = basic_request(request_url + '?q=John', access_token)
         self.assertEqual(response.status_code, 400)
 
+    def test_user_not_found_search_term(self):
+        bad_name_url = request_url + '?q=abcdef'
+        response = basic_request(bad_name_url, access_token)
+        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.json()['data'], [])
+
     def test_user_found_search_term(self):
         good_name_url = request_url + '?q=' + urllib.quote(config_json["good_name"])
         response = basic_request(good_name_url, access_token)
         self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(response.json()['data'], [])
+
+    def test_user_found_search_term_reversed(self):
+        name = config_json["good_name"]
+        reversed_name = " ".join(reversed(name.split()))
+        good_name_url = request_url + '?q=' + urllib.quote(reversed_name)
+        response = basic_request(good_name_url, access_token)
+        self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(response.json()['data'], [])
 
     def test_missing_q_search_term(self):
         response = basic_request(request_url, access_token)
@@ -30,23 +47,43 @@ class gateway_tests(unittest.TestCase):
         middle_name_url = request_url + "?q=" + urllib.quote(config_json["middle_cn_search"])
         response = basic_request(middle_name_url, access_token)
         self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(response.json()['data'], [])
 
     def test_partial_mail_search_term(self):
         partial_mail_url = request_url + "?q=" + urllib.quote(config_json["partial_email"])
         response = basic_request(partial_mail_url, access_token)
         self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(response.json()['data'], [])
+
+    def test_sanitize(self):
+        good_uid = "browtayl"
+        for bad_char in "()*&#":
+            response = basic_request(request_url, access_token, params=dict(q=good_uid + bad_char))
+            self.assertEquals(response.status_code, 200)
+            self.assertEquals(response.json()['data'][0]['attributes']['username'], good_uid)
+
+            response = basic_request(request_url, access_token, params=dict(q=bad_char + good_uid))
+            self.assertEquals(response.status_code, 200)
+            self.assertEquals(response.json()['data'][0]['attributes']['username'], good_uid)
+
+    def test_unicode(self):
+        unicode_name = 'Hernández'
+        response = basic_request(request_url, access_token, params=dict(q=unicode_name))
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.json()['data'][0]['attributes']['username'], 'hernand2')
 
     def test_osuuid_found(self):
         osuuid_url = request_url + "/" + config_json["good_osuuid"]
         response = basic_request(osuuid_url, access_token)
         self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(response.json()['data'], [])
 
-    def test_osuuid_bad_req(self):
+    def test_osuuid_bad_req_empty(self):
         osuuid_url = request_url + "/" + ""
         response = basic_request(osuuid_url, access_token)
         self.assertEquals(response.status_code, 400)
 
-    def test_osuuid_bad_req(self):
+    def test_osuuid_bad_req_invalid(self):
         osuuid_url = request_url + "/" + "!@#!@#12dqwd"
         response = basic_request(osuuid_url, access_token)
         self.assertEquals(response.status_code, 404)
@@ -57,11 +94,12 @@ class gateway_tests(unittest.TestCase):
         self.assertEquals(response.status_code, 401)
 
 if __name__ == '__main__':
-    options_tpl = ('-i', 'config_path')
     del_list = []
 
-    for i,config_path in enumerate(sys.argv):
-        if config_path in options_tpl:
+    config_path = None
+    for i, option in enumerate(sys.argv):
+        if option == '-i':
+            config_path = sys.argv[i+1]
             del_list.append(i)
             del_list.append(i+1)
 
