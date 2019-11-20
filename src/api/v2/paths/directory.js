64538629d38800ae282d2e71d1cb7ae70e6547fb
David Kalcic <kalcicd@oregonstate.edu>
2019-11-19 16:40:05 -0800

    return 400 if any query parameter is missing a value

--- src/api/v2/paths/directory.js
+++ src/api/v2/paths/directory.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import { errorHandler, errorBuilder } from 'errors/errors';
 
 import { getDirectories } from '../db/ldap/directory-dao';
@@ -10,6 +12,16 @@ import { getDirectories } from '../db/ldap/directory-dao';
  */
 const get = async (req, res) => {
   try {
+    const errors = [];
+    _.forEach(req.query, (value, key) => {
+      if (!value) {
+        errors.push(`Query parameter '${key}' must have a value`);
+      }
+    });
+    if (!_.isEmpty(errors)) {
+      return errorBuilder(res, 400, errors);
+    }
+
     const result = await getDirectories(req.query);
     if (!result) {
       return errorBuilder(res, 400, ['No query parameters specified.']);
