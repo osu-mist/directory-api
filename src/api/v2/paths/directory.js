0e05068af2d9ddea4171621321ee80177152565d
David Kalcic <kalcicd@oregonstate.edu>
2019-11-21 14:51:23 -0800

    use _.pickBy and _.reduce

--- src/api/v2/paths/directory.js
+++ src/api/v2/paths/directory.js
@@ -12,12 +12,12 @@ import { getDirectories } from '../db/ldap/directory-dao';
  */
 const get = async (req, res) => {
   try {
-    const errors = [];
-    _.forEach(req.query, (value, key) => {
-      if (!value) {
-        errors.push(`Query parameter '${key}' must have a value.`);
-      }
-    });
+    const valuelessParams = _.pickBy(req.query, (value) => !value && value !== 0);
+    const generateErrorString = (accumulator, value, key) => {
+      accumulator.push(`Query parameter '${key}' must have a value.`);
+      return accumulator;
+    };
+    const errors = _.reduce(valuelessParams, generateErrorString, []);
     if (!_.isEmpty(errors)) {
       return errorBuilder(res, 400, errors);
     }
